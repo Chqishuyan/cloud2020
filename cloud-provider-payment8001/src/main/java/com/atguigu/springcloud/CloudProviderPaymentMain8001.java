94eb888b731d113fd6102339a1197508a9cec146
shuyan <18205601657@163.com>
2020-04-06 18:13:56 +0800

    支付服务注册进zookeeper

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPaymentMain8001.java
@@ -3,15 +3,14 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shuyan.qi
  * @date 2020-04-05 18:21
  */
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
+//@EnableEurekaClient
+@EnableDiscoveryClient//该注解用于向使用了zookeeper或consul作为注册中心时注册服务
 public class CloudProviderPaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(CloudProviderPaymentMain8001.class, args);
