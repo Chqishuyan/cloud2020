a5fb06e826790280bfcd4d4d8cee5e09f2db0416
shuyan <18205601657@163.com>
2020-04-06 21:41:35 +0800

    订单服务注册到consul注册中心

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/web/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/web/OrderController.java
@@ -21,7 +21,8 @@ public class OrderController {
     //private static final String payment_base_url = "http://localhost:8001";
     //private static final String payment_eureka_url = "http://CLOUD-PAYMENT-SERVICE"; //eureka服务节点
 
-    private static final String payment_zookeeper_url = "http://cloud-payment-service";
+    //private static final String payment_consul_url = "http://cloud-payment-service";
+    private static final String payment_consul_url = "http://cloud-payment-service";
     @Resource
     private RestTemplate restTemplate;
 
@@ -29,13 +30,13 @@ public class OrderController {
     public CommonResult create(Payment payment){
         log.info("******* create payment={}",payment);
         //return restTemplate.postForObject(payment_eureka_url+"/payment/create",payment,CommonResult.class);
-        return restTemplate.postForObject(payment_zookeeper_url +"/payment/create",payment,CommonResult.class);
+        return restTemplate.postForObject(payment_consul_url +"/payment/create",payment,CommonResult.class);
     }
 
     @GetMapping(value = "/order/get/{id}")
     public CommonResult getById(@PathVariable("id")Long id){
         log.info("****** getById id={}",id);
         //return restTemplate.getForObject(payment_eureka_url+"/payment/get/"+id, CommonResult.class);
-        return restTemplate.getForObject(payment_zookeeper_url +"/payment/get/"+id, CommonResult.class);
+        return restTemplate.getForObject(payment_consul_url +"/payment/get/"+id, CommonResult.class);
     }
 }
