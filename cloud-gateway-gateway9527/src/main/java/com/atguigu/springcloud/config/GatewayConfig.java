ba1715f302dc50cff37bffb41bde207abc1b1974
shuyan <18205601657@163.com>
2020-04-11 14:08:03 +0800

    Gateway路由配置方式二:编码

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author shuyan.qi
+ * @date 2020-04-11 13:39
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator myRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        Builder routes = routeLocatorBuilder.routes();
+        routes.route("payment-service-1", r -> r.path("/payment/hystrix/ok/**").uri("http://localhost:8001"));
+        routes.route("payment-service-2", r -> r.path("/payment/get/**").uri("http://localhost:8002"));
+        return routes.build();
+    }
+}
