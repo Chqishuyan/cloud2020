613105cc69037ced6a416ac331ed48dc3a771026
shuyan <18205601657@163.com>
2020-04-11 13:36:34 +0800

    Gateway路由配置方式一:配置文件

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/CloudGatewayMain9527.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shuyan.qi
+ * @date 2020-04-11 12:54
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+public class CloudGatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayMain9527.class, args);
+    }
+}
--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/CloudProviderPaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/CloudProviderPaymentMain8002.java
@@ -3,14 +3,15 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shuyan.qi
  * @date 2020-04-05 18:21
  */
 @SpringBootApplication
-//@EnableEurekaClient
-@EnableDiscoveryClient
+@EnableEurekaClient
+//@EnableDiscoveryClient
 public class CloudProviderPaymentMain8002 {
     public static void main(String[] args) {
         SpringApplication.run(CloudProviderPaymentMain8002.class, args);
