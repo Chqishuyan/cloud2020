96afb1528199ae2db27976f34d4ffc6e1f2c0390
shuyan <18205601657@163.com>
2020-04-12 09:28:22 +0800

    Gateway断言(Predicates)

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author shuyan.qi
+ * @date 2020-04-12 09:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
